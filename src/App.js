6d0950ffe4e2a333e79ba2c10b589d77489fa2a9
sangeun-jo <dog8536@naver.com>
2021-06-18 21:29:31 +0900

    리액트 훅 완성

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
 import React, { useEffect, useState, useRef } from "react";
-
-const useFullscreen = (callback) => {
-  const element = useRef();
-  console.log(element);
-  const triggerFull = () => {
-    if (element.current) {
-      element.current.requestFullscreen();
-      if (callback && callback === "function") {
-        callback(true);
-      }
-    }
-  };
-  const exitFull = () => {
-    if (callback && callback === "function") {
-      callback(false);
-    }
-    const checkFullScreen = document.fullscreenElement;
-    if (checkFullScreen) {
-      document.exitFullscreen();
-    }
-  };
-  return { element, triggerFull, exitFull };
-};
+import useAxios from "./useAxios";
 
 const App = () => {
-  const onFullS = (isFull) => {
-    console.log(isFull ? "We are full" : "We are small");
-  };
-  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+  const { loading, data, refetch } = useAxios({
+    url: "https://yts-proxy.now.sh/list_movies.json"
+  });
   return (
     <div className="App">
-      <div ref={element}>
-        <img src="https://i.ibb.co/R6RwNxx/grape.jpg" alt="grape" />
-        <button onClick={exitFull}>exit fullscreen</button>
-      </div>
-      <button onClick={triggerFull}>Make fullscreen</button>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
